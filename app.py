7320ff5763ad2cf4734be562413c8e0a6b9fbc9f
Krutikakhamkar-7 <krutikakhamkar07@gmail.com>
2026-03-28 18:32:18 +0530

    Prepare for Railway deployment

--- app.py
+++ app.py
@@ -101,5 +101,5 @@ def clear_history():
 if __name__ == "__main__":
     import os
     init_db()
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
